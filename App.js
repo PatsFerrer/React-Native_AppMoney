710c03ba20efef3d366bc6956743ebe9ab1cd074
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-03 09:43:07 -0300

    tela home test

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { Home } from './src/pages/home/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Home />
+      {/* <Text>Olá, mundo</Text>
+      <Image src='' /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
